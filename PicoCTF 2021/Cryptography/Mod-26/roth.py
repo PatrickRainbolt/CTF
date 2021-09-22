beccc0b7d902141310515ca0be7631194de31106
Patrick Rainbolt <38919321+PatrickRainbolt@users.noreply.github.com>
2021-09-22 17:40:08 -0400

    Update roth.py

--- PicoCTF 2021/Cryptography/Mod-26/roth.py
+++ PicoCTF 2021/Cryptography/Mod-26/roth.py
@@ -4,8 +4,6 @@ import codecs
 import sys
 
 if len(sys.argv) > 1:
-    # phrase = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_GYpXOHqX}"
-
     print(codecs.decode(sys.argv[1], 'rot_13'))
 else:
     print "SYNTAX ERROR: Invalid Arguments!   roth.py {phrase}"
